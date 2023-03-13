b960b96d2eeb1457950ad8c49fa9210afc28f703
Achmad Rozy Priambodo <120998950+RozyyyyA@users.noreply.github.com>
2023-03-13 23:31:49 +0700

    Add files via upload

---
+++ TUGASPHP2/customer.php
@@ -0,0 +1,106 @@
+<?php
+include('koneksi.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>TUGASPHP2</title>
+    <link rel="stylesheet" href="stylesphp2.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+    <nav>
+        <h2>CLASSIC MODEL DATABASE</h2>
+        <ul>
+            <li><a href="#">Customers</a></li>
+            <li><a href="products.php">Products</a></li>
+        </ul>
+    </nav>
+
+    <div class="tabel-wrapper">
+        <h3>CUSTOMERS TABLE</h3>
+        <table>
+            <thead>
+                <tr>
+                    <th>Customer Number</th>
+                    <th>Customer Name</th>
+                    <th>Contact Last Name</th>
+                    <th>Contact First Name</th>
+                    <th>Phone</th>
+                    <th>Address Line 1</th>
+                    <th>Address Line 2</th>
+                    <th>City</th>
+                    <th>State</th>
+                    <th>Postal Code</th>
+                    <th>Country</th>
+                    <th>Sales Rep Employee Number</th>
+                    <th>Credit Limit</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $query = "SELECT * FROM customers";
+                $result = mysqli_query($conn, $query);
+
+                if (mysqli_num_rows($result) > 0) {
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        ?>
+                        <tr>
+                            <td>
+                                <?php echo $row['customerNumber']; ?>
+                            </td>
+                            <td>
+                                <?php echo $row['customerName']; ?>
+                            </td>
+                            <td>
+                                <?php echo $row['contactLastName']; ?>
+                            </td>
+                            <td>
+                                <?php echo $row['contactFirstName']; ?>
+                            </td>
+                            <td>
+                                <?php echo $row['phone']; ?>
+                            </td>
+                            <td>
+                                <?php echo $row['addressLine1']; ?>
+                            </td>
+                            <td>
+                                <?php echo $row['addressLine2']; ?>
+                            </td>
+                            <td>
+                                <?php echo $row['city']; ?>
+                            </td>
+                            <td>
+                                <?php echo $row['state']; ?>
+                            </td>
+                            <td>
+                                <?php echo $row['postalCode']; ?>
+                            </td>
+                            <td>
+                                <?php echo $row['country']; ?>
+                            </td>
+                            <td>
+                                <?php echo $row['salesRepEmployeeNumber']; ?>
+                            </td>
+                            <td>
+                                <?php echo $row['creditLimit']; ?>
+                            </td>
+                            <?php
+                    }
+                    mysqli_free_result($result);
+                } else {
+                    echo "Data tidak ada";
+                }
+                mysqli_close($conn);
+                ?>
+                </tr>
+            </tbody>
+        </table>
+        <h6>copyright@2023 - Achmad Rozy</h6>
+    </div>
+</body>
+
+</html>
