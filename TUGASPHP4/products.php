e6f271ee62cbf76753e0f7cee59e079dec72bd80
Achmad Rozy Priambodo <120998950+RozyyyyA@users.noreply.github.com>
2023-03-23 20:44:53 +0700

    Add files via upload

---
+++ TUGASPHP4/products.php
@@ -0,0 +1,87 @@
+<?php
+include('koneksi.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>TUGAS INSERT</title>
+    <link rel="stylesheet" href="stylesphp4.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+    <nav>
+        <h2>CLASSIC MODEL DATABASE</h2>
+        <ul>
+            <li><a href="customer.php">Customers</a></li>
+            <li><a href="#">Products</a></li>
+            <li><a href="insert product.php">Insert</a></li>
+        </ul>
+    </nav>
+
+    <div class="tabel-wrapper">
+        <h3>PRODUCTS TABLE</h3>
+        <table>
+            <thead>
+                <tr>
+                    <th>Product Code</th>
+                    <th>Product Line</th>
+                    <th>Product Scale</th>
+                    <th>Product Vendor</th>
+                    <th>Product Description</th>
+                    <th>Quantity In Stock</th>
+                    <th>Buy Price</th>
+                    <th>MSRP</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $query = "SELECT * FROM products";
+                $result = mysqli_query($conn, $query);
+
+                if (mysqli_num_rows($result) > 0) {
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        ?>
+                        <tr>
+                            <td>
+                                <?php echo $row['productCode']; ?>
+                            </td>
+                            <td>
+                                <?php echo $row['productLine']; ?>
+                            </td>
+                            <td>
+                                <?php echo $row['productScale']; ?>
+                            </td>
+                            <td>
+                                <?php echo $row['productVendor']; ?>
+                            </td>
+                            <td>
+                                <?php echo $row['productDescription']; ?>
+                            </td>
+                            <td>
+                                <?php echo $row['quantityInStock']; ?>
+                            </td>
+                            <td>
+                                <?php echo $row['buyPrice']; ?>
+                            </td>
+                            <td>
+                                <?php echo $row['MSRP']; ?>
+                            </td>
+                            <?php
+                    }
+                    mysqli_free_result($result);
+                } else {
+                    echo "Data tidak ada";
+                }
+                mysqli_close($conn);
+                ?>
+                </tr>
+            </tbody>
+        </table>
+        <h6>copyright@2023 - Achmad Rozy</h6>
+    </div>
+</body>
+
+</html>
